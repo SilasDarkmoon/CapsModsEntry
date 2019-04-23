efba90b38ace517829438925b6c9c33e256c945e
Silas <hdss3609@gmail.com>
2019-04-23 16:50:58 +0800

    Link Package To Mod

---
+++ Editor/ResManagerEditorEntry.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Capstones.UnityEditorEx
+{
+    [InitializeOnLoad]
+    public static class ResManagerEditorEntry
+    {
+        static ResManagerEditorEntry()
+        {
+            LinkPackageToMod();
+        }
+
+        [MenuItem("Mods/Link Package to Mod", priority = 200000)]
+        public static void LinkPackageToMod()
+        {
+            var req = UnityEditor.PackageManager.Client.List(true);
+            EditorApplication.QueuePlayerLoopUpdate();
+            EditorApplication.update = () =>
+            {
+                if (req.IsCompleted)
+                {
+                    EditorApplication.update = null;
+
+                    Dictionary<string, string> linked = new Dictionary<string, string>();
+                    bool linkupdated = false;
+                    if (System.IO.File.Exists("EditorOutput/Runtime/linked-package.txt"))
+                    {
+                        try
+                        {
+                            var lines = System.IO.File.ReadAllLines("EditorOutput/Runtime/linked-package.txt");
+                            if (lines != null)
+                            {
+                                for (int i = 0; i < lines.Length; ++i)
+                                {
+                                    var line = lines[i];
+                                    if (line != null)
+                                    {
+                                        var parts = line.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                                        if (parts != null && parts.Length >= 2)
+                                        {
+                                            linked[parts[0]] = parts[1];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogException(e);
+                        }
+                    }
+
+                    foreach (var package in req.Result)
+                    {
+                        if (package.status == UnityEditor.PackageManager.PackageStatus.Available
+                            && (package.source == UnityEditor.PackageManager.PackageSource.Embedded || package.source == UnityEditor.PackageManager.PackageSource.Git || package.source == UnityEditor.PackageManager.PackageSource.Local)
+                            )
+                        {
+                            var path = package.resolvedPath;
+                            var mod = System.IO.Path.GetFileNameWithoutExtension(path);
+                            if (System.IO.Directory.Exists(path + "/Link~"))
+                            {
+                                bool isuptodate = linked.ContainsKey(package.name) && linked[package.name] == path;
+                                if (!isuptodate)
+                                {
+                                    linkupdated = true;
+                                    UnlinkOrDeleteDir("Assets/Mod/" + mod + "/Content");
+                                    for (int i = 0; i < UniqueSpecialFolders.Length; ++i)
+                                    {
+                                        var usdir = UniqueSpecialFolders[i];
+                                        UnlinkOrDeleteDir("Assets/" + usdir + "/Mods/" + mod + "/Content");
+                                    }
+                                    if (linked.ContainsKey(package.name))
+                                    {
+                                        var oldmod = System.IO.Path.GetFileNameWithoutExtension(linked[package.name]);
+                                        UnlinkOrDeleteDir("Assets/Mod/" + oldmod + "/Content");
+                                        for (int i = 0; i < UniqueSpecialFolders.Length; ++i)
+                                        {
+                                            var usdir = UniqueSpecialFolders[i];
+                                            UnlinkOrDeleteDir("Assets/" + usdir + "/Mods/" + oldmod + "/Content");
+                                        }
+                                    }
+                                }
+                                LinkPackageToMod(package);
+                            }
+                        }
+                    }
+
+                    if (linkupdated)
+                    {
+                        System.IO.Directory.CreateDirectory("EditorOutput/Runtime");
+                        using (var sw = new System.IO.StreamWriter("EditorOutput/Runtime/linked-package.txt"))
+                        {
+                            foreach (var kvp in linked)
+                            {
+                                sw.Write(kvp.Key);
+                                sw.Write('|');
+                                sw.Write(kvp.Value);
+                                sw.WriteLine();
+                            }
+                        }
+                        AssetDatabase.Refresh();
+                    }
+                }
+                else
+                {
+                    EditorApplication.QueuePlayerLoopUpdate();
+                }
+            };
+        }
+
+        private static readonly string[] UniqueSpecialFolders = new[] { "Plugins", "Standard Assets" };
+
+        private static void UnlinkOrDeleteDir(string path)
+        {
+            if (ResManagerEditorEntryUtils.IsDirLink(path))
+            {
+                ResManagerEditorEntryUtils.DeleteDirLink(path);
+            }
+            else
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                else if (System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.Delete(path, true);
+                }
+            }
+        }
+
+        private static void LinkPackageToMod(UnityEditor.PackageManager.PackageInfo package)
+        {
+            var path = package.resolvedPath;
+            var mod = System.IO.Path.GetFileNameWithoutExtension(path);
+            if (System.IO.Directory.Exists(path + "/Link~/Mod"))
+            {
+                var link = "Assets/Mod/" + mod + "/Content";
+                if (!System.IO.Directory.Exists(link) && !System.IO.File.Exists(link))
+                {
+                    System.IO.Directory.CreateDirectory("Assets/Mod/" + mod);
+                    ResManagerEditorEntryUtils.MakeDirLink(link, path + "/Link~/Mod");
+                }
+            }
+            for (int i = 0; i < UniqueSpecialFolders.Length; ++i)
+            {
+                var usdir = UniqueSpecialFolders[i];
+                var link = "Assets/" + usdir + "/Mods/" + mod + "/Content";
+                var target = path + "/Link~/" + usdir;
+                if (System.IO.Directory.Exists(target) && !System.IO.Directory.Exists(link) && !System.IO.File.Exists(link))
+                {
+                    System.IO.Directory.CreateDirectory("Assets/" + usdir + "/Mods/" + mod);
+                    ResManagerEditorEntryUtils.MakeDirLink(link, target);
+                }
+            }
+        }
+    }
+}
---
+++ Editor/ResManagerEditorEntryUtils.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Capstones.UnityEditorEx
+{
+    public static class ResManagerEditorEntryUtils
+    {
+        public static void HideFile(string path)
+        {
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("chflags", "hidden \"" + path + "\"");
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(path))
+                {
+                    var di = new System.IO.DirectoryInfo(path);
+                    di.Attributes |= System.IO.FileAttributes.Hidden;
+                }
+                else
+                {
+                    var fi = new System.IO.FileInfo(path);
+                    if (fi.Exists)
+                    {
+                        fi.Attributes |= System.IO.FileAttributes.Hidden;
+                    }
+                }
+            }
+        }
+        public static void UnhideFile(string path)
+        {
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("chflags", "nohidden \"" + path + "\"");
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(path))
+                {
+                    var di = new System.IO.DirectoryInfo(path);
+                    di.Attributes &= ~System.IO.FileAttributes.Hidden;
+                }
+                else
+                {
+                    var fi = new System.IO.FileInfo(path);
+                    if (fi.Exists)
+                    {
+                        fi.Attributes &= ~System.IO.FileAttributes.Hidden;
+                    }
+                }
+            }
+        }
+        public static bool IsFileHidden(string path)
+        {
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("ls", "-lOd \"" + path + "\"");
+                si.UseShellExecute = false;
+                si.RedirectStandardOutput = true;
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+                var output = p.StandardOutput.ReadToEnd();
+                if (string.IsNullOrEmpty(output))
+                {
+                    return false;
+                }
+                output = output.Trim();
+                if (output.EndsWith(path, System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    output = output.Substring(0, output.Length - path.Length).Trim();
+                }
+                var idsplit = output.IndexOfAny(new[] { '/', '\\' });
+                if (idsplit >= 0)
+                {
+                    output = output.Substring(0, idsplit);
+                }
+                return output.Contains("hidden");
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(path))
+                {
+                    var di = new System.IO.DirectoryInfo(path);
+                    return (di.Attributes & System.IO.FileAttributes.Hidden) != 0;
+                }
+                else
+                {
+                    var fi = new System.IO.FileInfo(path);
+                    if (fi.Exists)
+                    {
+                        return (fi.Attributes & System.IO.FileAttributes.Hidden) != 0;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public static void DeleteDirLink(string path)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("cmd", "/C \"rmdir \"" + path.Replace('/', '\\') + "\"\"");
+                si.CreateNoWindow = true;
+                si.UseShellExecute = false;
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+            }
+            else
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("rm", "\"" + path + "\"");
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+            }
+        }
+        public static void MakeDirLink(string link, string target)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("cmd", "/C \"mklink /D \"" + link.Replace('/', '\\') + "\"" + " \"" + target.Replace('/', '\\') + "\"\"");
+                si.CreateNoWindow = true;
+                si.UseShellExecute = false;
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+
+                if (p.ExitCode != 0)
+                {
+                    si = new System.Diagnostics.ProcessStartInfo("cmd", "/C \"mklink /J \"" + link.Replace('/', '\\') + "\"" + " \"" + (target.StartsWith(".") ? System.IO.Path.GetDirectoryName(link.Replace('/', '\\').TrimEnd('\\')) + "\\" : "") + target.Replace('/', '\\') + "\"\"");
+                    si.CreateNoWindow = true;
+                    si.UseShellExecute = false;
+                    p = System.Diagnostics.Process.Start(si);
+                    p.WaitForExit();
+                }
+            }
+            else
+            {
+                var si = new System.Diagnostics.ProcessStartInfo("ln", "-s \"" + target + "\"" + " \"" + link + "\"");
+                var p = System.Diagnostics.Process.Start(si);
+                p.WaitForExit();
+            }
+        }
+        public static bool IsDirLink(string path)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                var di = new System.IO.DirectoryInfo(path);
+                return di.Exists && (di.Attributes & System.IO.FileAttributes.ReparsePoint) == System.IO.FileAttributes.ReparsePoint;
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(path) || System.IO.File.Exists(path))
+                {
+                    var di = new System.IO.DirectoryInfo(path);
+                    return (di.Attributes & System.IO.FileAttributes.ReparsePoint) == System.IO.FileAttributes.ReparsePoint;
+                }
+                return false;
+            }
+        }
+
+        public static bool ExecuteProcess(System.Diagnostics.ProcessStartInfo si)
+        {
+            si.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            si.UseShellExecute = false;
+            si.RedirectStandardOutput = true;
+            si.RedirectStandardError = true;
+            si.CreateNoWindow = true;
+
+            var process = new System.Diagnostics.Process();
+            process.StartInfo = si;
+
+            process.OutputDataReceived += (s, e) => WriteProcessOutput(s as System.Diagnostics.Process, e.Data, false);
+
+            process.ErrorDataReceived += (s, e) => WriteProcessOutput(s as System.Diagnostics.Process, e.Data, true);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                Debug.LogErrorFormat("Error when execute process {0} {1}", si.FileName, si.Arguments);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static void WriteProcessOutput(System.Diagnostics.Process p, string data, bool isError)
+        {
+            if (!string.IsNullOrEmpty(data))
+            {
+                string processName = System.IO.Path.GetFileName(p.StartInfo.FileName);
+                if (Application.platform == RuntimePlatform.OSXEditor)
+                {
+                    if (processName == "wine" || processName == "mono")
+                    {
+                        var parts = p.StartInfo.Arguments.Split(' ');
+                        if (parts != null && parts.Length > 0)
+                        {
+                            processName = System.IO.Path.GetFileName(parts[0]);
+                        }
+                    }
+                }
+                if (!isError)
+                {
+                    Debug.LogFormat("[{0}] {1}", processName, data);
+                }
+                else
+                {
+                    Debug.LogErrorFormat("[{0} Error] {1}", processName, data);
+                }
+            }
+        }
+
+        public static string GetFileMD5(string path)
+        {
+            try
+            {
+                byte[] hash = null;
+                using (var stream = System.IO.File.OpenRead(path))
+                {
+                    using (var md5 = System.Security.Cryptography.MD5.Create())
+                    {
+                        hash = md5.ComputeHash(stream);
+                    }
+                }
+                if (hash == null || hash.Length <= 0) return "";
+                var sb = new System.Text.StringBuilder();
+                for (int i = 0; i < hash.Length; ++i)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+            catch { }
+            return "";
+        }
+        public static long GetFileLength(string path)
+        {
+            try
+            {
+                var f = new System.IO.FileInfo(path);
+                return f.Length;
+            }
+            catch { }
+            return 0;
+        }
+    }
+}
