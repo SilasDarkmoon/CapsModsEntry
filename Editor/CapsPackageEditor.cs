acc032d873cb1385a58f30007ce2d2b580fe5d9c
Silas <hdss3609@gmail.com>
2019-05-14 18:06:42 +0800

    CapsPackageEditor

---
+++ Editor/CapsPackageEditor.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Capstones.UnityEngineEx;
+
+namespace Capstones.UnityEditorEx
+{
+    public static class CapsPackageEditor
+    {
+        private static Dictionary<string, UnityEditor.PackageManager.PackageInfo> _Packages;
+        public static Dictionary<string, UnityEditor.PackageManager.PackageInfo> Packages { get { return _Packages; } }
+        private static event Action _OnPackagesChanged = () => { };
+        public static event Action OnPackagesChanged
+        {
+            add
+            {
+                _OnPackagesChanged += value;
+                if (_Packages != null)
+                {
+                    value();
+                }
+            }
+            remove
+            {
+                _OnPackagesChanged -= value;
+            }
+        }
+
+        public static void RefreshPackages()
+        {
+            var req = UnityEditor.PackageManager.Client.List(true);
+            EditorBridge.TerminableUpdate += () =>
+            {
+                if (req.IsCompleted)
+                {
+                    var newinfos = new Dictionary<string, UnityEditor.PackageManager.PackageInfo>();
+                    foreach (var package in req.Result)
+                    {
+                        newinfos[package.name] = package;
+                    }
+                    if (_Packages == null || PackagesChanged(_Packages, newinfos))
+                    {
+                        _Packages = newinfos;
+                        _OnPackagesChanged();
+                    }
+                    //else
+                    //{
+                    //    _Packages = newinfos;
+                    //}
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+
+        [MenuItem("Mods/Force Refresh Package", priority = 200000)]
+        public static void ForceRefreshPackages()
+        {
+            _Packages = null;
+            RefreshPackages();
+        }
+
+        private class PackageMonitor : AssetPostprocessor
+        {
+            private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+            {
+                if (_Packages == null)
+                {
+                    RefreshPackages();
+                    return;
+                }
+                if (importedAssets != null)
+                {
+                    for (int i = 0; i < importedAssets.Length; ++i)
+                    {
+                        var asset = importedAssets[i];
+                        if (asset.StartsWith("Packages/"))
+                        {
+                            var pname = asset.Substring("Packages/".Length);
+                            var index = pname.IndexOf('/');
+                            if (index > 0)
+                            {
+                                pname = pname.Substring(0, index);
+                                if (!_Packages.ContainsKey(pname))
+                                {
+                                    RefreshPackages();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (deletedAssets != null)
+                {
+                    for (int i = 0; i < deletedAssets.Length; ++i)
+                    {
+                        var asset = deletedAssets[i];
+                        if (asset.StartsWith("Packages/"))
+                        {
+                            var pname = asset.Substring("Packages/".Length);
+                            var index = pname.IndexOf('/');
+                            if (index > 0)
+                            {
+                                pname = pname.Substring(0, index);
+                                if (asset == "Packages/" + pname + "/package.json")
+                                {
+                                    RefreshPackages();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (movedAssets != null)
+                {
+                    for (int i = 0; i < movedAssets.Length; ++i)
+                    {
+                        var asset = movedAssets[i];
+                        if (asset.StartsWith("Packages/"))
+                        {
+                            var pname = asset.Substring("Packages/".Length);
+                            var index = pname.IndexOf('/');
+                            if (index > 0)
+                            {
+                                pname = pname.Substring(0, index);
+                                if (!_Packages.ContainsKey(pname))
+                                {
+                                    RefreshPackages();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (movedFromAssetPaths != null)
+                {
+                    for (int i = 0; i < movedFromAssetPaths.Length; ++i)
+                    {
+                        var asset = movedFromAssetPaths[i];
+                        if (asset.StartsWith("Packages/"))
+                        {
+                            var pname = asset.Substring("Packages/".Length);
+                            var index = pname.IndexOf('/');
+                            if (index > 0)
+                            {
+                                pname = pname.Substring(0, index);
+                                if (asset == "Packages/" + pname + "/package.json")
+                                {
+                                    RefreshPackages();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool PackagesChanged(Dictionary<string, UnityEditor.PackageManager.PackageInfo> src, Dictionary<string, UnityEditor.PackageManager.PackageInfo> dst)
+        {
+            if (src.Count != dst.Count)
+            {
+                return true;
+            }
+            foreach (var kvp in dst)
+            {
+                UnityEditor.PackageManager.PackageInfo old;
+                if (!src.TryGetValue(kvp.Key, out old))
+                {
+                    return true;
+                }
+                return kvp.Value.resolvedPath != old.resolvedPath;
+            }
+            return false;
+        }
+    }
+}
--- Editor/ResManagerEditorEntry.cs
+++ Editor/ResManagerEditorEntry.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Capstones.UnityEngineEx;
 
 namespace Capstones.UnityEditorEx
 {
@@ -12,117 +13,101 @@ namespace Capstones.UnityEditorEx
     {
         static ResManagerEditorEntry()
         {
-            LinkPackageToMod();
-        }
-
-        [MenuItem("Mods/Link Package to Mod", priority = 200000)]
-        public static void LinkPackageToMod()
-        {
-            var req = UnityEditor.PackageManager.Client.List(true);
-            EditorApplication.QueuePlayerLoopUpdate();
-            EditorApplication.update = () =>
+            CapsPackageEditor.OnPackagesChanged += () =>
             {
-                if (req.IsCompleted)
+                Dictionary<string, string> linked = new Dictionary<string, string>();
+                bool linkupdated = false;
+                if (System.IO.File.Exists("EditorOutput/Runtime/linked-package.txt"))
                 {
-                    EditorApplication.update = null;
-
-                    Dictionary<string, string> linked = new Dictionary<string, string>();
-                    bool linkupdated = false;
-                    if (System.IO.File.Exists("EditorOutput/Runtime/linked-package.txt"))
+                    try
                     {
-                        try
+                        var lines = System.IO.File.ReadAllLines("EditorOutput/Runtime/linked-package.txt");
+                        if (lines != null)
                         {
-                            var lines = System.IO.File.ReadAllLines("EditorOutput/Runtime/linked-package.txt");
-                            if (lines != null)
+                            for (int i = 0; i < lines.Length; ++i)
                             {
-                                for (int i = 0; i < lines.Length; ++i)
+                                var line = lines[i];
+                                if (line != null)
                                 {
-                                    var line = lines[i];
-                                    if (line != null)
+                                    var parts = line.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                                    if (parts != null && parts.Length >= 2)
                                     {
-                                        var parts = line.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-                                        if (parts != null && parts.Length >= 2)
-                                        {
-                                            linked[parts[0]] = parts[1];
-                                        }
+                                        linked[parts[0]] = parts[1];
                                     }
                                 }
                             }
                         }
-                        catch (Exception e)
-                        {
-                            Debug.LogException(e);
-                        }
                     }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
+                }
 
-                    HashSet<string> existingmods = new HashSet<string>();
-                    foreach (var package in req.Result)
+                HashSet<string> existingmods = new HashSet<string>();
+                foreach (var package in CapsPackageEditor.Packages.Values)
+                {
+                    if (package.status == UnityEditor.PackageManager.PackageStatus.Available
+                        && (package.source == UnityEditor.PackageManager.PackageSource.Embedded || package.source == UnityEditor.PackageManager.PackageSource.Git || package.source == UnityEditor.PackageManager.PackageSource.Local)
+                        )
                     {
-                        if (package.status == UnityEditor.PackageManager.PackageStatus.Available
-                            && (package.source == UnityEditor.PackageManager.PackageSource.Embedded || package.source == UnityEditor.PackageManager.PackageSource.Git || package.source == UnityEditor.PackageManager.PackageSource.Local)
-                            )
+                        var path = package.resolvedPath;
+                        var mod = System.IO.Path.GetFileNameWithoutExtension(path);
+                        if (System.IO.Directory.Exists(path + "/Link~"))
                         {
-                            var path = package.resolvedPath;
-                            var mod = System.IO.Path.GetFileNameWithoutExtension(path);
-                            if (System.IO.Directory.Exists(path + "/Link~"))
+                            existingmods.Add(mod);
+                            bool isuptodate = linked.ContainsKey(package.name) && linked[package.name] == path;
+                            if (!isuptodate)
                             {
-                                existingmods.Add(mod);
-                                bool isuptodate = linked.ContainsKey(package.name) && linked[package.name] == path;
-                                if (!isuptodate)
+                                UnlinkMod(mod);
+                                if (linked.ContainsKey(package.name))
                                 {
-                                    UnlinkMod(mod);
-                                    if (linked.ContainsKey(package.name))
-                                    {
-                                        var oldmod = System.IO.Path.GetFileNameWithoutExtension(linked[package.name]);
-                                        UnlinkMod(oldmod);
-                                    }
-                                    linked[package.name] = path;
-                                    linkupdated = true;
+                                    var oldmod = System.IO.Path.GetFileNameWithoutExtension(linked[package.name]);
+                                    UnlinkMod(oldmod);
                                 }
-                                LinkPackageToMod(package);
+                                linked[package.name] = path;
+                                linkupdated = true;
                             }
+                            LinkPackageToMod(package);
                         }
                     }
-                    if (linked.Count != existingmods.Count)
+                }
+                if (linked.Count != existingmods.Count)
+                {
+                    List<string> keystodel = new List<string>();
+                    foreach (var kvp in linked)
                     {
-                        List<string> keystodel = new List<string>();
+                        if (!existingmods.Contains(kvp.Key))
+                        {
+                            keystodel.Add(kvp.Key);
+                            var mod = System.IO.Path.GetFileNameWithoutExtension(kvp.Value);
+                            UnlinkMod(mod);
+                        }
+                    }
+                    linkupdated = true;
+                    for (int i = 0; i < keystodel.Count; ++i)
+                    {
+                        linked.Remove(keystodel[i]);
+                    }
+                }
+
+                if (linkupdated)
+                {
+                    System.IO.Directory.CreateDirectory("EditorOutput/Runtime");
+                    using (var sw = new System.IO.StreamWriter("EditorOutput/Runtime/linked-package.txt"))
+                    {
                         foreach (var kvp in linked)
                         {
-                            if (!existingmods.Contains(kvp.Key))
-                            {
-                                keystodel.Add(kvp.Key);
-                                var mod = System.IO.Path.GetFileNameWithoutExtension(kvp.Value);
-                                UnlinkMod(mod);
-                            }
-                        }
-                        linkupdated = true;
-                        for (int i = 0; i < keystodel.Count; ++i)
-                        {
-                            linked.Remove(keystodel[i]);
+                            sw.Write(kvp.Key);
+                            sw.Write('|');
+                            sw.Write(kvp.Value);
+                            sw.WriteLine();
                         }
                     }
-
-                    if (linkupdated)
-                    {
-                        System.IO.Directory.CreateDirectory("EditorOutput/Runtime");
-                        using (var sw = new System.IO.StreamWriter("EditorOutput/Runtime/linked-package.txt"))
-                        {
-                            foreach (var kvp in linked)
-                            {
-                                sw.Write(kvp.Key);
-                                sw.Write('|');
-                                sw.Write(kvp.Value);
-                                sw.WriteLine();
-                            }
-                        }
-                        AssetDatabase.Refresh();
-                    }
-                }
-                else
-                {
-                    EditorApplication.QueuePlayerLoopUpdate();
+                    AssetDatabase.Refresh();
                 }
             };
+            CapsPackageEditor.RefreshPackages();
         }
 
         private static readonly string[] UniqueSpecialFolders = new[] { "Plugins", "Standard Assets" };
@@ -169,7 +154,7 @@ namespace Capstones.UnityEditorEx
                 var link = "Assets/Mods/" + mod;
                 if (!System.IO.Directory.Exists(link) && !System.IO.File.Exists(link))
                 {
-                    System.IO.Directory.CreateDirectory("Assets/Mods/" + mod);
+                    System.IO.Directory.CreateDirectory("Assets/Mods/");
                     ResManagerEditorEntryUtils.MakeDirLink(link, path + "/Link~/Mod");
                 }
             }
